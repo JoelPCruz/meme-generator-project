40290448e36e62f2075586a8e8cb338f1116d8f2
Joel Cruz <104873706+Joerican305@users.noreply.github.com>
2023-08-04 05:35:32 -0400

    adding notes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // VARIABLES
 document.addEventListener('DOMContentLoaded', () => {  // fires before doc loads into browsers. going into the doc, when it loads execute all of the code in the script.
-let imgDiv = document.querySelector('.meme-container'); // selecting the div element where the img will be placed
-let imgInput = document.querySelector('#link'); // selecting the link input element
+let imgDiv = document.querySelector('.meme-container'); // selecting elementsß
+let imgInput = document.querySelector('#link'); 
 let topTextInp = document.querySelector('#topText');
 let botTextInp = document.querySelector('#botText');
-let form = document.querySelector('.form'); // selecting the form
+let form = document.querySelector('.form'); 
 
 
 //FUNCTIONS
@@ -20,10 +20,10 @@ imgDiv.addEventListener('click', (e) =>{  // listening for an event on the butto
 
 form.addEventListener("submit", (e) => { // when the submit button is clicked... arrow function. e = event
     e.preventDefault(); // stops default behavior when submit is executed
-    if(imgInput.value === '' || topTextInp.value === '' || botTextInp.value === '') {
-        return alert('please enter a value for each field');
-    } else {
-    imgDiv.innerHTML += `
+    if(imgInput.value === '' || topTextInp.value === '' || botTextInp.value === '') { // if any of the 3 input values are equal to an empty string
+        return alert('please enter a value for each field'); // alert the user
+    } else { // anything else... add the html
+    imgDiv.innerHTML += ` 
         <div class='img-container' style='background-image: url(${imgInput.value})'>
             <h2>${topTextInp.value}</h2>
             <h3>${botTextInp.value}</h3>           
@@ -40,6 +40,11 @@ form.addEventListener("submit", (e) => { // when the submit button is clicked...
 
 
 });
+
+// some pictures
+//https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6nxPD6yU1axSLKfibWbeX0ySWvgxiT-MIkQ&usqp=CAU
+//https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyJqodST_QCvdAPWqPfz3SP8Pxb4RFhpC0ZQ&usqp=CAU
+
 
 // window.addEventListener('load', () => { //only fires when everything is loaded
 // });
